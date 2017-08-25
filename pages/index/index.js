e9f1c15476f3a3d1688acd92b1de2f8b80770c82
Guo Weixuan <cnmozzie@gmail.com>
2017-08-25 22:35:06 +0800

    change constructure of index

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,10 +5,17 @@ var app = getApp()
 //panel.js
 var base64 = require("../images/base64");
 
+//navbar.js
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
 Page({
   data: {
-    motto: 'This is my motto!',
-    userInfo: {}
+    //navbar.js
+    tabs: ["主页", "活动通知", "我的"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0
+    //userInfo: {}
   },
   //事件处理函数
   bindViewTap: function() {
@@ -34,11 +41,28 @@ Page({
 
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    //app.getUserInfo(function(userInfo){
       //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+      //that.setData({
+        //userInfo:userInfo
+      //})
+    //})
+
+    //navbar.js
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  },
+  //navbar.js
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   }
 })
